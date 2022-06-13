5c3d1d532ab65ebfe6093c5bb3dfe8af92217d23
Đỗ Minh Trí <90955782+mitidevus@users.noreply.github.com>
2022-06-13 15:16:47 +0700

    commit

---
+++ src/Todo/index.js
@@ -0,0 +1,45 @@
+import { useReducer, useRef } from 'react'
+import reducer, { initState } from './reducer'
+import { setJob, addJob, deleteJob } from './actions'
+
+
+function App() {
+    const [state, dispatch] = useReducer(reducer, initState)
+    // Destructuring lấy ra cho gọn
+    const { job, jobs } = state
+
+    const inputRef = useRef()
+
+    const handleSubmit = () => {
+        dispatch(addJob(job))
+        dispatch(setJob(''))
+        inputRef.current.focus()
+    }
+
+    return (
+        <div style={{ padding: '0 20px' }}>
+            <h3>Todo</h3>
+            <input
+                ref={inputRef}
+                value={job}
+                placeholder='Enter todo...'
+                onChange={e => {
+                    dispatch(setJob(e.target.value))
+                }}
+            />
+            <button onClick={handleSubmit}>Add</button>
+            <ul>
+                {jobs.map((job, index) => (
+                    <li key={index}>
+                        {job}
+                        <span onClick={() => dispatch(deleteJob(index))}>
+                            &times;
+                        </span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default App;
